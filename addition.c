67f431bffa14890e48a9571d32218b804af58490
Jessica Stephens <jeslstep1@gmail.com>
2021-08-11 13:11:52 -0500

    c practice, addtion function and greetting function

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(viod)
+{
+    // Declare the variables
+    int num;
+    int num1;
+    // Input the integer
+    printf("Enter the first integer: ");
+    scanf("%d", &num);
+    // Input the integer
+    printf("Enter the second integer: ");
+    scanf("%d", &num1);
+
+    // Output the first integer plus the second integer
+    printf("\nEntered integer addition: %d", num + num1);
+}
