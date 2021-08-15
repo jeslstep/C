009bc7bb57d7d757e53c4c513fa133f157bb57e0
Jessica Stephens <jeslstep1@gmail.com>
2021-08-14 20:58:18 -0500

    adding run and updating complile scripts

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
-gcc -Wall agree.c -o agree
+gcc -Wall src/addition/add.c -o src/addition/add
+gcc -Wall src/conditionals/agree.c -o src/conditionals/agree
+gcc -Wall src/division/divide.c -o src/division/divide
+gcc -Wall src/greeting/hello.c -o src/greeting/hello
+gcc -Wall src/loops/loops.c -o src/loops/loops
---
+++ run.sh
@@ -0,0 +1,5 @@
+./src/greeting/hello
+./src/addition/add
+./src/division/divide
+./src/loops/loops
+./src/conditionals/agree
